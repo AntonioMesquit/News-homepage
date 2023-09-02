3b657c9ebd8b45ac9bd00a3407312b772c995d1a
AntonioMesquit <140577539+AntonioMesquit@users.noreply.github.com>
2023-09-02 13:31:37 -0300

    up final

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 console.log("oi")
 const burguer = document.querySelector(".menu");
-
+const menu = document.querySelector(".menus");
 burguer.addEventListener('click' , function(){
     const ativo = this.classList.contains("active")
     burguer.classList.add("active")
